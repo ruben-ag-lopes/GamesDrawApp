9712db5ba99b59277f9b0c5f9ac44774ab7b2a46
Ruben Lopes <ruben.ag.lopes@gmail.com>
2022-02-16 19:26:45 +0000

    Random Generator e corpo HTML

--- Script.py
+++ Script.py
@@ -1,2 +1,16 @@
-print('This is the title')
-print('This is the subtitle')
+import random 
+
+# Números Euromilhões
+print(random.sample(range(1,50), 5))
+
+# Estrelas Totoloto
+print(random.sample(range(1,12), 2))
+
+# Números Totoloto
+print(random.sample(range(1,49), 5))
+
+# Número da sorte Totoloto
+print(random.sample(range(1,13), 1))
+
+# Número da lotaria clássica
+print(random.sample(range(2062,99129), 1))
